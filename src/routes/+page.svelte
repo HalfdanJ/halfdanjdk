65f0a5f7976e29b9afac68214ec725b7172f7b7f
Jonas Jongejan <jonas@halfdanj.dk>
2025-11-26 12:23:52 -0500

    new project added

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -15,17 +15,28 @@
 </script>
 
 <div class="mt-4 mb-12">
-	<p class="mb-4">
-		I'm a Creative Technologist / Fullstack Software Engineer / Hot Glue Gun Developer, or whatever
-		you want to call it, previously at the Google Creative Lab in New York. I've worked on a wide
-		range of projects, often touching on making science and technology more accessible to everyone,
-		or adding a little bit of delight to topics such as machine learning and AR.
+	<p class="mb-8 text-base">
+		Fullstack Software Engineer / Creative Technologist – specialized in early stage products and
+		experimentation utilizing emerging technologies.
 	</p>
-	<p>
-		Before Google I worked on interactive video set designs for theatre and dance pieces and created
-		installations using video tracking, robots and lights. Lastly, I worked for a few years for the
-		Danish Broadcasting Corporation, innovating new ways of making graphics for the tv.
-	</p>
+
+	<ul class="my-2 flex flex-col gap-3 text-sm">
+		<li>
+			<strong>2023-present:</strong> Leading development of various early stage products at startups
+			that utilize new capabilities of AI.
+		</li>
+		<li>
+			<strong>2016-2023:</strong> Leading technical projects at Google Creative Lab that amplify and
+			enable new opportunities from Google Research and beyond.
+		</li>
+		<li>
+			<strong>2013-2016:</strong> Building out new novel software for broadcast graphics rendering.
+		</li>
+		<li>
+			<strong>2008-2011:</strong> Exploring new technological possibilities with software and hardware
+			for theatre and dance pieces.
+		</li>
+	</ul>
 </div>
 
 <h1 class="mb-4 text-xl font-semibold sm:text-2xl">Selected Work</h1>
@@ -37,9 +48,9 @@
 		{@const posts = data.groupedPosts[group]}
 		<div class="grid grid-cols-2 gap-6 gap-y-12 sm:flex sm:flex-wrap">
 			{#each posts as post, i}
-				<a href={post.slug} class="group max-w-[12rem] no-underline hover:text-black">
+				<a href={post.slug} class="group max-w-[12rem] no-underline outline-none hover:text-black">
 					<div
-						class="relative overflow-clip rounded-lg shadow transition-all group-hover:-translate-y-0.5 group-hover:shadow-lg"
+						class="relative overflow-clip border-1 border-gray-200 transition-all group-hover:scale-105 group-hover:border-8 group-hover:border-amber-200 group-focus:scale-105 group-focus:border-8 group-focus:border-amber-200"
 					>
 						{#if post.previewVideo}
 							<HoverVideoElement videoSrc={post.previewVideo} imageSrc={post.previewImg} />
